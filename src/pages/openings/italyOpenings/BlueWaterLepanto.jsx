8b04d73da5b75b9e7be25d4e9317ad817f063668
Duncan Brewster <duncanwbrewster@gmail.com>
2022-06-14 22:49:00 -0700

    Edited Lepanto Description

--- src/pages/openings/italyOpenings/BlueWaterLepanto.jsx
+++ src/pages/openings/italyOpenings/BlueWaterLepanto.jsx
@@ -22,13 +22,13 @@ const BlueWaterLepanto = () => {
                 F Nap - ION <br />A Ven - H <br />A Rom - Apu
               </p>
               <p>
-                Manus Hand's Fall 1901 continuation of the Blue Water Opening.
-                The Austrian (who opened to the Adriatic) sails to the Ionian in
-                Fall of 1901 to be the victim of an arranged dislodgement by
-                Italian fleets from Naples and Tunis in Spring 1902. Austria's
-                fleet then surprises the Turkish player by retreating eastward
-                to form part of the convoy chain that carries an Italian army to
-                Turkey.
+                Opens the same as the standard Lepanto in the Spring but diverts
+                in the Fall. Also, the Austrian (who opened to the Adriatic)
+                sails to the Ionian in Fall of 1901 to be the victim of an
+                arranged dislodgement by Italian fleets from Naples and Tunis in
+                Spring 1902. Austria's fleet then surprises the Turkish player
+                by retreating eastward to form part of the convoy chain that
+                carries an Italian army to Turkey.
               </p>
               <a
                 href="http://uk.diplom.org/pouch/Zine/W1999A/Hand/bwl.html"
@@ -36,7 +36,9 @@ const BlueWaterLepanto = () => {
                 rel="noreferrer"
                 style={{ textDecoration: "none" }}
               >
-                <p className="author">Click here for full article by Manus Hand</p>
+                <p className="author">
+                  Click here for full article by Manus Hand
+                </p>
               </a>
             </div>
           </div>
